af76abe7acab85e8bfdf8f7297fbf1bf5826da5f
mia <m.09048030369@gmail.com>
2022-07-13 11:53:51 +1000

    add link

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,6 +6,26 @@ const Header = () => {
           <h1 className='p-header__title'>React TODO</h1>
           <span className='p-header__emoji'> &#129412;</span>
         </div>
+        <ul className='c-tag'>
+          <a
+            href='http://vuetodo.s3-website-ap-northeast-1.amazonaws.com/'
+            className='c-tag__wrapper'
+          >
+            <li className='c-tag__item'>Vue.js ver</li>
+          </a>
+          <a
+            href='http://jquerytodo.s3-website-ap-northeast-1.amazonaws.com/'
+            className='c-tag__wrapper'
+          >
+            <li className='c-tag__item'>jQuery ver</li>
+          </a>
+          <a
+            href='http://backbonetodo.s3-website-ap-northeast-1.amazonaws.com/'
+            className='c-tag__wrapper'
+          >
+            <li className='c-tag__item'>Backbone.js ver</li>
+          </a>
+        </ul>
       </header>
     </>
   );
